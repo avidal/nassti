0e2c069a600f14487f2eb1b47a54b95e93609371
Alex Vidal <alex.vidal@gmail.com>
2015-02-06 17:29:16 -0600

    Use arrays instead of a hash for triggers

--- examples/local.js
+++ examples/local.js
@@ -8,19 +8,17 @@ session.configure({
   port: 4000
 });
 
-session.trigger(`^By what name do you wish to be known?`, function() {
+session.trigger(/^By what name do you wish to be known?/, function() {
   // Read the next line from the client for the character name then emit
   // an event
   session.read(function(input) {
-    console.log("Read input:", input);
     session.emit('username', input);
   });
 
   return "WHAT'S NAME PRECIOUS?? ";
 });
 
-session.trigger(`^(\\d+)H (\\d+)M (\\d+)V > $`, function(line, rx, match) {
-  console.log("HP", match[1], "MP", match[2], "MV", match[3]);
+session.trigger(/^(\d+)H (\d+)M (\d+)V > $/, function(line, rx, match) {
   session.emit('prompt', { hp: match[1], mp: match[2], mv: match[3] });
 });
 
