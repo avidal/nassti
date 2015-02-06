66ec0a0c03664a7cf09feb9ce226a1473e80bfc4
Alex Vidal <alex.vidal@gmail.com>
2015-02-06 15:33:15 -0600

    add session.read to get the next line of input, allow triggers to change output text

--- examples/local.js
+++ examples/local.js
@@ -8,8 +8,15 @@ session.configure({
   port: 4000
 });
 
-session.trigger(`^By what name`, function(line, done) {
-  console.log("Matched on", line);
+session.trigger(`^By what name do you wish to be known?`, function() {
+  // Read the next line from the client for the character name then emit
+  // an event
+  session.read(function(input) {
+    console.log("Read input:", input);
+    session.emit('username', input);
+  });
+
+  return "WHAT'S NAME PRECIOUS?? ";
 });
 
 module.exports = session;
